4cc156d832a1786b179fe768cf90e3c41aa985d8
Szymon Leyk <leyas@wp.pl>
2020-09-18 08:52:57 +0200

    stworzenie projektu spring boot, dodanie modelu Todo

---
+++ src/main/java/pl/szymonleyk/restclient/RestClientApplication.java
@@ -0,0 +1,11 @@
+package pl.szymonleyk.restclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestClientApplication.class, args);
+    }
+}
